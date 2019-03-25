64f9f2f55ee883f273052f3d3332bb4ef07788e2
Nat Keane <ntlkeane.nk@gmail.com>
2019-03-24 19:47:17 -0500

    'pics'

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -19,7 +19,7 @@ Vue.component('app-header', {
                 </ul>
               </div>
             </nav>
-        </header>    
+        </header>
     `,
     data: function() {}
 });
@@ -37,13 +37,89 @@ Vue.component('app-footer', {
             year: (new Date).getFullYear()
         }
     }
-})
-
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
-    }
 });
 
+const NewsList = Vue.component('news-list', {
+    template: `
+
+      <div class="news">
+        <h2>News</h2>
+        <ul class="news__list">
+        <li v-for="article in articles" class="news__item">{{ article.title }}</br>
+          <img class="img" :src=" article.urlToImage " :alt="image"> </br>
+          {{ article.description}}</br></li>
+      </ul>
+      </div>
+
+      <div class="form-inline d-flex justify-content-center">
+           <div class="form-group mx-sm-3 mb-2">
+                    <label class="sr-only" for="search">Search</label>
+                   <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                   <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+            </div>
+      </div>
+    `,
+    created: function(){
+      let self=this;
+       fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=1f0221d86d734ee1acf9a274ce87c1f5')
+      // fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<your-api-key>')
+       .then(function(response) {
+         return response.json();
+       })
+       .then(function(data) {
+         console.log(data);
+         self.articles=data.articles;
+       });
+     },
+     data: function() {
+       return {
+         articles: [],
+         searchTerm: ''
+      }
+   },
+   methods: {
+        searchNews: function() {
+        let self = this;
+        fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=<your-api-key>')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          self.articles = data.articles;
+        });
+      }
+    }
+
+});
+
+const Home = Vue.component('home', {
+     template: `
+     <div class="home">
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>{{ welcome }}</h1>
+     </div>
+     `,
+     data: function() {
+       return {
+          welcome: 'Hello World! Welcome to VueJS'
+        }
+      }
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
+});
+
+const app = new Vue({
+  el: '#app',
+  router
+//let app = new Vue({
+    //el: '#app',
+    //data: {
+    //    welcome: 'Hello World! Welcome to VueJS'
+    });
